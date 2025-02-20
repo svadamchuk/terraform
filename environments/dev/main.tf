9b669dfdf2604a91976f136a943543f6eca7adfe
sergey adamchuk <sergey.adamchuk@bttcloud.com>
2025-02-20 20:41:08 +0200

    add ALB security group ID to networking module in dev environment

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -6,6 +6,7 @@ module "networking" {
   public_subnets    = ["10.0.1.0/24", "10.0.2.0/24"]
   private_subnets   = ["10.0.3.0/24", "10.0.4.0/24"]
   availability_zones = ["eu-central-1a", "eu-central-1b"]
+  alb_security_group_id = module.security.alb_security_group_id
 }
 
 module "security" {
