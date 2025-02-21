52c7500ed37ca7262cc6a44bccc4b3e2fc9ec385
sergey adamchuk <sergey.adamchuk@bttcloud.com>
2025-02-21 12:23:33 +0200

    refactor: enable S3 bucket versioning and remove unused variable; add index.html for AWS infrastructure overview

--- modules/storage/variables.tf
+++ modules/storage/variables.tf
@@ -7,9 +7,3 @@ variable "bucket_name" {
   description = "Name of the S3 bucket"
   type        = string
 }
-
-variable "versioning_enabled" {
-  description = "Enable versioning for the S3 bucket"
-  type        = bool
-  default     = true
-}
